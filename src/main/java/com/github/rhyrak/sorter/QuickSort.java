cbd7c32e8d30c156557b8f80b3c9f8686f021a4e
Furkan-Demir <87570239+GH0ST-TF141@users.noreply.github.com>
2023-11-05 21:47:09 +0300

    Update QuickSort.java

--- src/main/java/com/github/rhyrak/sorter/QuickSort.java
+++ src/main/java/com/github/rhyrak/sorter/QuickSort.java
@@ -2,9 +2,42 @@ package com.github.rhyrak.sorter;
 
 import com.github.rhyrak.model.NISTEntry;
 
-public class QuickSort implements Sorter<NISTEntry>{
+public class QuickSort implements Sorter<NISTEntry> {
     @Override
     public void sort(NISTEntry[] c) {
-        // TODO: Implement Quick Sort
+        quickSort(c, 0, c.length - 1); //Starting the quick-sort 
+    }
+
+    //Implementing the quick-sort algorithm
+    private void quickSort(NISTEntry[] arr, int left, int right) {
+        if (left < right) {
+            int pivotIndex = partition(arr, left, right); //Get the correct position of the pivot element
+
+            quickSort(arr, left, pivotIndex - 1); //Recursively sorting the left subarray
+            quickSort(arr, pivotIndex + 1, right); //Recursively sorting the right subarray
+        }
+    }
+
+    //Choosing the pivot element and arrange elements smaller than pivot on the left and greater on the right
+    private int partition(NISTEntry[] arr, int left, int right) {
+        NISTEntry pivot = arr[right]; // Choose the pivot element
+        int i = left - 1;
+
+        for (int j = left; j < right; j++) {
+            if (arr[j].compareTo(pivot) <= 0) {
+                i++;
+                swap(arr, i, j); //Swapping elements to arrange them around the pivot
+            }
+        }
+        swap(arr, i + 1, right); 
+
+        return i + 1;
+    }
+
+    //Swapping two elements in the array
+    private void swap(NISTEntry[] arr, int i, int j) {
+        NISTEntry temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
