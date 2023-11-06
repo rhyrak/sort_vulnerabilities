bac1923b28bc390844c43375b8cbd98c269f6a15
He-Is-HaZaRdOuS <wither725@gmail.com>
2023-11-06 14:21:07 +0300

    Include contributors' names in common source files

--- src/main/java/com/github/rhyrak/sorter/InsertionSort.java
+++ src/main/java/com/github/rhyrak/sorter/InsertionSort.java
@@ -1,3 +1,11 @@
+/*
+19050111022, FURKAN DEMİR
+20050111011, İBRAHİM BAHÇA
+20050111034, MERTER ÇOBAN
+20050111008, SELÇUK GENÇAY
+21050141038, YOUSIF HARITH SUHAIL SUHAIL
+ */
+
 package com.github.rhyrak.sorter;
 
 public class InsertionSort<T extends Comparable<T>> implements Sorter<T>{
