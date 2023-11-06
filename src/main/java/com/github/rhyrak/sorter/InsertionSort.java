dc8e23cf9844aa961bdb5fbc8da2991fe2a7c24a
rogrammer <96201583+rogrammer@users.noreply.github.com>
2023-11-06 12:20:58 +0300

    Update InsertionSort.java

--- src/main/java/com/github/rhyrak/sorter/InsertionSort.java
+++ src/main/java/com/github/rhyrak/sorter/InsertionSort.java
@@ -3,14 +3,13 @@ package com.github.rhyrak.sorter;
 import com.github.rhyrak.model.NISTEntry;
 
 public class InsertionSort<T extends Comparable<T>> implements Sorter<T>{
-    T[] local;
+    private T[] local;
     @Override
     public void sort(T[] c) {
         // TODO: Implement Insertion Sort
         local = c.clone();
         T value;
         int size = local.length;
-        System.out.println(size);
         for(int i = 1; i < size; i++) {
             value = local[i];
             for(int j = i - 1; j >= 0; j--) {
@@ -26,7 +25,7 @@ public class InsertionSort<T extends Comparable<T>> implements Sorter<T>{
         }
     }
     // swap entries
-    public void swap(int index1, int index2) {
+    private void swap(int index1, int index2) {
         T temp = local[index1];
         local[index1] = local[index2];
         local[index2] = temp;
