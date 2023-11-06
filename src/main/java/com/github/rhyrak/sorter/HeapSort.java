d66e051b26cbaaacfc5527829d0d9031d6af8cce
rogrammer <96201583+rogrammer@users.noreply.github.com>
2023-11-06 11:48:37 +0300

    Update HeapSort.java

--- src/main/java/com/github/rhyrak/sorter/HeapSort.java
+++ src/main/java/com/github/rhyrak/sorter/HeapSort.java
@@ -3,85 +3,70 @@ package com.github.rhyrak.sorter;
 import com.github.rhyrak.model.NISTEntry;
 
 public class HeapSort<T extends Comparable<T>> implements Sorter<T> {
-    T[] local;
+    private T[] local;
+    private int heapsize = 0;
     @Override
     public void sort(T[] c) {
         // TODO: Implement Heap Sort
         local = c.clone();
         this.build();
-        for(int i = local.length -1; i > 0; i--) {
-           this.swap(0, i);
-           this.max_heapify(0);
+        for(int i = local.length -1; i >= 0; i--) {
+            swap(i, 0);
+            heapsize--;
+            max_heapify(0);
         }
-
         int size = local.length;
         for(int i = 0; i < size; i++){
             c[i] = local[i];
         }
-//        NISTEntry temp = c[0];
-//        c[0] = c[1];
-//        c[1] = temp;
     }
     
     //build a max heap
-    public void build() {
+    private void build() {
         int size = local.length;
-        
-        for(int i = size / 2 - 1; i > 0; i--) {
+        heapsize = size;
+        for(int i = size / 2 - 1; i >= 0; i--) {
              max_heapify(i);
         }
-        
     }
     
     //check bounds
-    public boolean inBound(int index) {
-        return (index < local.length && index >= 0 ) ? true : false;
+    private boolean inBound(int index) {
+        return (index < heapsize && index >= 0 ) ? true : false;
     }
     
     //get left child
-    public int getLeft(int index) {
+    private int getLeft(int index) {
         if(inBound(index*2 +1))
             return index * 2 + 1;
         return -1;
     }
     
     //get right child
-    public int getRight(int index) {
+    private int getRight(int index) {
         if(inBound(index*2 + 2))
             return index * 2 + 2;
         return -1;
     }
     
-    //get parent
-    public int getParent(int index) {
-        if(inBound((index - 1) / 2))
-            return (index - 1) / 2;
-        return -1;
-    }
-    
     //swap entries
-    public void swap(int index1, int index2) {
-        if(!inBound(index1) && !inBound(index2))
-            return;
+    private void swap(int index1, int index2) {
         T temp = local[index1];
         local[index1] = local[index2];
         local[index2] = temp;
     }
     
     //provide heap properties
-    public void max_heapify(int index) {
+    private void max_heapify(int index) {
         if(getRight(index) == -1 && getLeft(index) == -1)
             return;
       
-        T max = local[index];
         int maxIndex = index;
         
-        if(getRight(index) != -1 && max.compareTo(local[getRight(index)]) < 0) {
-            max = local[getRight(index)];
+        if(getRight(index) != -1 && local[maxIndex].compareTo(local[getRight(index)]) < 0) {
             maxIndex = getRight(index);
         }
-        if(getLeft(index) != -1 && max.compareTo(local[getLeft(index)]) < 0) {
-            max = local[getLeft(index)];
+        if(getLeft(index) != -1 && local[maxIndex].compareTo(local[getLeft(index)]) < 0) {
             maxIndex = getLeft(index);
         }
         
@@ -92,4 +77,5 @@ public class HeapSort<T extends Comparable<T>> implements Sorter<T> {
            
         max_heapify(maxIndex);
     }
+
 }
